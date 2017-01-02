8a24c344c0ca22ab9256d0b81fa7c8563cf18c91
EKillick <edwinkillick@gmail.com>
2017-01-02 15:03:26 +0000

    Added comments to script

--- mywc_make/results.sh
+++ mywc_make/results.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
+#Shows usage help if the command is used incorrectly
 usage(){ echo "Usage: $0 [-r <string>] [-f <string(s)>]" 1>&2; exit 1; }
 while getopts ":r:f:" opts; do
+  #Stores list of filenames as a variable
   list="$OPTARG"
   case "${opts}" in
     r)
       make
-      eval ${OPTARG};;
+      eval ${OPTARG}
+      ;;
     f)
+      #Iterates through the filenames
       for arg in $list
         do
-          echo "----------"
           cat "$arg"
-      done;;
+          printf "\n\n\n" #Newlines for formatting
+      done
+      ;;
     *)
-      usage;;
+      #Prints usage help
+      usage
+      ;;
     esac
 done
