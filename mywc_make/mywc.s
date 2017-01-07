95ad6b39892263e789b07893e7e1b09f45c3bbc9
EKillick <edwinkillick@gmail.com>
2017-01-07 18:22:25 +0000

    Assembly file updated

--- mywc_make/mywc.s
+++ mywc_make/mywc.s
@@ -4,3 +4,12 @@
 .text
 .balign 4
 .global main
+
+@include stdio?
+
+main:	@int ch
+	mov r2, #0		@int linecount = 0
+	mov r3, #0		@int charcount = 0
+	mov r4, #0		@int flag = 0
+
+loop:	@while not EOF 
