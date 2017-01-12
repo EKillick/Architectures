4c4ca09d33c58dc61cc84c2521c10cc3fb04d2b3
EKillick <edwinkillick@gmail.com>
2017-01-12 20:09:42 +0000

    updated asm file

--- mywc_make/mywc.s
+++ mywc_make/mywc.s
@@ -1,27 +1,30 @@
 @ mywc.s
 @ an assembly translation of mywc.c
 
+@ Data Section
+.data
+linecount:
+	.word 0			@int linecount = 0
+charcount:
+	.word 0			@int charcount = 0
+flag:
+	.word 0			@int flag = 0
+
+
+@ Code Section
 .text
 
 .code 16			@using Thumb 16-bit
 .align 2			@2 byte aligned instructions
 
-addr_linecount:	.word linecount
-addr_charcount: .word 
 
-thumb_function:
-	mov r2, #0		@int linecount = 0
-	mov r3, #0		@int charcount = 0
-	mov r4, #0		@int flag = 0
+loop:				@while not EOF
+	bl getchar
+	mov r1, r0
 
-loop:	@while not EOF
 
 main:
 
-.data
-linecount:
-	.word 0
-charcount:
-	.word 0
-flag:
-	.word 0
+addr_linecount:	.word linecount
+addr_charcount: .word charcount
+addr_flag: .word flag
