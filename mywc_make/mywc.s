7b02fa38ae52982a5bd026a41945e147ba1e77bd
EKillick <edwinkillick@gmail.com>
2017-01-13 02:30:09 +0000

    added to implementation of  mywc.s

--- mywc_make/mywc.s
+++ mywc_make/mywc.s
@@ -1,30 +1,43 @@
 @ mywc.s
 @ an assembly translation of mywc.c
 
-@ Data Section
-.data
-linecount:
-	.word 0			@int linecount = 0
-charcount:
-	.word 0			@int charcount = 0
-flag:
-	.word 0			@int flag = 0
-
 
 @ Code Section
 .text
+.global main
+.global getchar
 
 .code 16			@using Thumb 16-bit
 .align 2			@2 byte aligned instructions
 
-
-loop:				@while not EOF
-	bl getchar
-	mov r1, r0
-
-
 main:
+	mov r4, #0		@int linecount = 0
+	mov r5, #0		@int wordcount = 0
+	mov r6, #0		@int charcount = 0
+	mov r7, #0		@int flag = 0
 
-addr_linecount:	.word linecount
-addr_charcount: .word charcount
-addr_flag: .word flag
+loop:
+	bl getchar		@while ((ch = getchar()) != EOF)
+	mov r1, r0		@puts output of getchar in r1
+	cmn r1, #1		@cmn used to compare to -1
+	beq return		@ends while loop
+
+	cmp r1, #32		@if ch = ' '
+	beq else_word
+	cmp r1, #10		@if ch = '\n'
+	beq else_word
+
+	add r6, #1		@wordcount++
+	mov r7, #1		@flag = 1
+
+	b loop
+
+else_word:
+	cmp r7, #0		@if flag = 0
+	add r6, #1		@charcount++
+	bne loop
+	add r5, #1		@wordcount++
+	mov r7, #1		@flag = 1
+
+return:
+
