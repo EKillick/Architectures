8060c7aab454e9efb3e89cebfbe736f74c68669c
EKillick <edwinkillick@gmail.com>
2017-01-15 20:09:06 +0000

    question 1 complete

---
+++ mywc_asm/results.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Shows usage help if the command is used incorrectly
+usage(){ echo "Usage: $0 [-r <string>] [-f <string(s)>]" 1>&2; exit 1; }
+while getopts ":r:f:" opts; do
+  #Stores list of filenames as a variable
+  list="$OPTARG"
+  case "${opts}" in
+    r)
+      make
+      eval ${OPTARG}
+      ;;
+    f)
+      #Iterates through the filenames
+      for arg in $list
+        do
+          printf "\n" #Newlines for formatting
+          cat "$arg"
+          printf "\n"
+      done
+      ;;
+    *)
+      #Prints usage help
+      usage
+      ;;
+    esac
+done
